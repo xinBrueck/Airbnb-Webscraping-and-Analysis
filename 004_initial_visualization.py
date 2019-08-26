01298754b317127c357e53dc2d14930c6507b822
yang4055 <yang4055@umn.edu>
2019-08-25 22:00:07 -0500

    dashboard feature correlation plot

--- 004_initial_visualization.py
+++ 004_initial_visualization.py
@@ -2,6 +2,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
 import pandas as pd
 import numpy as np
@@ -9,21 +11,38 @@ import numpy as np
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MINTY])
 
 # Load data
-df = data = pd.read_csv("./data/seattle_individual_listing_details.csv",
-            sep='|', quotechar= '"', encoding= 'utf8')
+dt = pd.read_csv("./data/seattle_individual_listing_details_cleaned.csv", sep='|', quotechar= '"',
+encoding= 'utf8', index_col=0)
 
+##define glable varialbles
 
-# dtype = {'amendities': str, 'availableDts': str, 'id':str,
-# 'listingHouseType':str, 'listingLoc': str, 'listingNm':str,
-# 'neighborhoodSummary':str, 'numBaths':str, 'numBedrooms': str,
-# 'numGuests' : str, 'price': str,
-# 'numBeds': np.float64, 'reviewStar' : str, 'reviewTotal' : np.float64,
-# 'listingHouseTypeGroup' : str, 'numBathNew': np.float64, 'sharedBath' : np.float64,
-# 'numGuestsNew': np.float64, 'priceNew' : np.float64, 'availableDtsAug' : np.float64,
-# 'availableDtsSep': np.float64, 'availableDtsNov':np.float64, 'amenityString':str,
-# 'amenityBasic':str, 'amenityFamily':str, 'amenityFacilities':str, 'amenityDining':str,
-# 'amenityAccess':str, 'amenityLogistics': str, 'amenityBB': str,
-# 'amenitySafety':str, 'amenityNotIn': str},
+NUMERIC_FEATURES = ['P', 'NR', 'TAD']
+CATEGORICAL_FEATURES = ['HT', 'NBe', 'NBa', 'SB', 'NG', 'RR']
+COLNAME_DIST = {
+    'P': 'priceNew',
+    'NR': 'reviewTotal',
+    'TAD': 'totalAvailableDts',
+    'HT':'listingHouseTypeGroup',
+    'NBed': 'numBeds',
+    'NBer': 'numBedrooms',
+    'NBa': 'numBathNew',
+    'SB': 'sharedBath',
+    'RR':'reviewStar',
+    'NG': 'numGuestsNew'
+}
+
+LABEL_DIST = {
+    'P': 'Lising Price',
+    'NR': 'Number of Reviews',
+    'TAD': 'Total Available Dates',
+    'HT':'Housing Type',
+    'NBed': 'Number of Beds',
+    'NBer': 'Number of Bedrooms',
+    'NBa': 'Number of Bath',
+    'SB': 'Shared Bath',
+    'RR':'Review Ratings',
+    'NG': 'Number of Guests'
+}
 
 # Create app layout
 app.layout = html.Div(
@@ -53,47 +72,61 @@ app.layout = html.Div(
                         [
                             dbc.Card([
                                 dbc.CardBody([
-                                    html.H5(
-                                        "Filter by listing price range:",
-                                        className="control_label",
-                                    ),
-                                    dcc.RangeSlider(
-                                        id="price_slider",
-                                        min=0,
-                                        max=250,
-                                        value= [0, 250],
-                                        step = 50,
-                                        marks={
-                                            0: {'label': '$0'},
-                                            50: {'label': '$50'},
-                                            100: {'label': '$100'},
-                                            150: {'label': '$150'},
-                                            200: {'label': '$200'},
-                                            250: {'label': '$250'}
-                                        },
-                                        className="dcc_control",
+                                    html.H5("Choose the housing attribute: X, Y, Z", className="control_label"),
+                                    dcc.Dropdown(
+                                        id='drop-down-x',
+                                        options=[
+                                            {'label': 'House Type', 'value': 'HT'},
+                                            {'label': 'Number of Beds', 'value': 'NBed'},
+                                            {'label': 'Number of Bedrooms', 'value': 'NBer'},
+                                            {'label': 'Number of Baths', 'value': 'NBa'},
+                                            {'label': 'Shared Bath', 'value': 'SB'},
+                                            {'label': 'Number of Guests', 'value': 'NG'},
+                                            {'label': 'Number of Reviews', 'value': 'NR'},
+                                            {'label': 'Review Ratings', 'value': 'RR'},
+                                            {'label': 'Price', 'value': 'P'},
+                                            {'label': 'Total Available Dates', 'value': 'TAD'},
+                                        ],
+                                        value='HT'
                                     ),
 
                                     html.Br(),
-                                    html.Br(),
-                                    html.Br(),
-                                    html.H5("Choose the housing attribute:", className="control_label"),
-                                    dbc.DropdownMenu(
-                                        [
-                                            dbc.DropdownMenuItem("House Type", id = '0'),
-                                            dbc.DropdownMenuItem("Number of Beds", id = '1'),
-                                            dbc.DropdownMenuItem("Number of Baths", id = '2'),
-                                            dbc.DropdownMenuItem("Shared Bath", id = '3'),
-                                            dbc.DropdownMenuItem("Number of Guests", id = '4'),
-                                            dbc.DropdownMenuItem(divider=True),
-                                            dbc.DropdownMenuItem("Number of Reviews", id = '5'),
-                                            dbc.DropdownMenuItem("Review Rating", id = '6'),
-                                            dbc.DropdownMenuItem(divider=True),
-                                            dbc.DropdownMenuItem("Price", id = '7'),
+
+                                    dcc.Dropdown(
+                                        id='drop-down-y',
+                                        options=[
+                                            {'label': 'House Type', 'value': 'HT'},
+                                            {'label': 'Number of Beds', 'value': 'NBed'},
+                                            {'label': 'Number of Bedrooms', 'value': 'NBer'},
+                                            {'label': 'Number of Baths', 'value': 'NBa'},
+                                            {'label': 'Shared Bath', 'value': 'SB'},
+                                            {'label': 'Number of Guests', 'value': 'NG'},
+                                            {'label': 'Number of Reviews', 'value': 'NR'},
+                                            {'label': 'Review Ratings', 'value': 'RR'},
+                                            {'label': 'Price', 'value': 'P'},
+                                            {'label': 'Total Available Dates', 'value': 'TAD'},
                                         ],
-                                        label="Housing Feature",
-                                        color="success",
-                                    )
+                                        value='P'
+                                    ),
+
+                                    html.Br(),
+
+                                    dcc.Dropdown(
+                                        id='drop-down-z',
+                                        options=[
+                                            {'label': 'House Type', 'value': 'HT'},
+                                            {'label': 'Number of Beds', 'value': 'NBed'},
+                                            {'label': 'Number of Bedrooms', 'value': 'NBer'},
+                                            {'label': 'Number of Baths', 'value': 'NBa'},
+                                            {'label': 'Shared Bath', 'value': 'SB'},
+                                            {'label': 'Number of Guests', 'value': 'NG'},
+                                            {'label': 'Number of Reviews', 'value': 'NR'},
+                                            {'label': 'Review Ratings', 'value': 'RR'},
+                                            {'label': 'Price', 'value': 'P'},
+                                            {'label': 'Total Available Dates', 'value': 'TAD'},
+                                        ],
+                                        value='NBer'
+                                    ),
 
                                 ])
                             ])
@@ -125,5 +158,89 @@ app.layout = html.Div(
     ],
     id="mainContainer")
 
+##make graphs
+@app.callback(
+    Output('attribute_graph', 'figure'),
+    [Input('drop-down-x', 'value'),
+     Input('drop-down-y', 'value'),
+     Input('drop-down-z', 'value')])
+
+def update_figure(feature_x, feature_y, feature_z):
+    types=dt[COLNAME_DIST[feature_z]].unique()
+    data=[]
+
+    for type in types:
+        dt_type=dt[dt[COLNAME_DIST[feature_z]]==type]
+
+        data.append(
+            go.Scatter(
+                x=dt_type[COLNAME_DIST[feature_x]],
+                y=dt_type[COLNAME_DIST[feature_y]],
+                mode='markers',
+                marker={
+                    'size': 10,
+                    'color' : 'rgba(255, 182, 193, .9)',
+                    'line': {'width': 2, 'color': 'rgba(152, 0, 0, .8)'}
+                },
+                name=feature_z+" "+str(type)
+            )
+        )
+
+
+    # scatters = go.Scatter(
+    #     x=dt[COLNAME_DIST[feature_x]],
+    #     y=dt[COLNAME_DIST[feature_y]],
+    #     mode='markers',
+    #     marker={
+    #         'size': 10,
+    #         'color' : 'rgba(255, 182, 193, .9)',
+    #         'line': {'width': 2, 'color': 'rgba(152, 0, 0, .8)'}
+    #     }
+    # )
+
+    if feature_x in NUMERIC_FEATURES:
+        layout = go.Layout(
+            xaxis={
+                'title': LABEL_DIST[feature_x],
+            },
+            yaxis={
+                'title': LABEL_DIST[feature_y],
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+    else: #get the mean for each categorical group
+        average_by_group = dt.groupby([COLNAME_DIST[feature_x]]).agg({COLNAME_DIST[feature_y]: np.mean}).reset_index()
+
+        lineOfBestFit = go.Scatter(
+            x=average_by_group[COLNAME_DIST[feature_x]],
+            y=average_by_group[COLNAME_DIST[feature_y]],
+            mode='lines',
+        )
+
+        layout = go.Layout(
+            xaxis={
+                'title': LABEL_DIST[feature_x],
+                'categoryorder': 'array',
+                'categoryarray': average_by_group[COLNAME_DIST[feature_x]]
+            },
+            yaxis={
+                'title': LABEL_DIST[feature_y],
+            },
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            hovermode='closest'
+        )
+
+        data = data.append(lineOfBestFit)
+
+
+
+
+    return {
+        'data': data,
+        'layout':layout
+    }
+
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, port = 8069)
